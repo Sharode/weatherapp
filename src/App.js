be71709543582f4bd7f7069bac11250080f3ef7e
Neil Williams <Williams.Sharode@gmail.com>
2020-01-30 13:03:52 -0600

    logic complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 const api = {
   key: '000b7c246f3b0d54300ff5f8306acc67',
-  base: 'https:/api.openweathermap.org/data/2.5/'
+  base: 'https:/api.openweathermap.org/data/2.5/find?'
 }
+
 function App() {
   const [query, setQuery] = useState('')
   const [weather, setWeather] = useState({})
+  const [metrics, setMetrics] = useState('imperial')
 
-  const search = evt => {
+  const search = async (evt) => {
     if (evt.key === 'Enter') {
-      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
-        .then(res => res.json())
-        .then(result => {
-          console.log(result)
-
-          setWeather(result);
-          setQuery('')
-        })
-
+      const response = await fetch(`${api.base}q=${query}&units=metric&APPID=${api.key}`)
+      const result = await response.json()
+      setWeather(result);
+      setQuery('')
     }
   }
 
@@ -33,23 +30,47 @@ function App() {
 
     return `${day} ${date} ${month} ${year}`
   }
+
+  const handleClick = async (e) => {
+    metrics === 'imperial' ? setMetrics('metric') : setMetrics('imperial')
+  }
+
+  const temp = () => {
+    console.log(weather.list[0].main.temp)
+    console.log(weather)
+    if (metrics === 'metric') {
+      return (Math.floor(weather.list[0].main.temp))
+    }
+    if (metrics === 'imperial')
+      return Math.floor(((weather.list[0].main.temp * (1.8)) + 32))
+
+  }
+
   return (
-    <div className={(typeof weather.main != 'undefined') ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
+    <div className={(weather.message === 'accurate') ? ((weather.list[0].main.temp > 16) ? 'app warm' : 'app') : 'app'}>
       <main>
         <div className="search-box">
           <input type="" className='search-bar' placeholder='search ....' onChange={e => setQuery(e.target.value)} value={query} onKeyPress={search} />
+          <button
+            className={metrics === 'metric' ? 'true' : 'false'}
+            disabled={metrics !== 'metric'}
+            onClick={(e) => handleClick(e)}>Fahrenheit°</button>
+          <button
+            className={metrics !== 'metric' ? 'true' : 'false'}
+            disabled={metrics === 'metric'}
+            onClick={(e) => handleClick(e)}>Celcius°</button>
         </div>
-        {(typeof weather.main !== 'undefined') ? (
+        {(weather.message === "accurate" && weather.list.length > 0) ? (
           <div>
             <div className="location-box">
-              <div className="location">{weather.name}, {weather.sys.country}</div>
+              <div className="location">{weather.list[0].name}, {weather.list[0].sys.country}</div>
               <div className="date">{dateBuilder(new Date())}</div>
             </div>
             <div className="weather-box">
               <div className="temp">
-                {Math.round(weather.main.temp)}°
-          </div>
-              <div className="weather">{weather.weather[0].main}</div>
+                {temp()}°{metrics === 'metric' ? 'C' : 'F'}
+              </div>
+              <div className="weather">{weather.list[0].weather[0].main}</div>
             </div>
           </div>
         ) : ('')}
